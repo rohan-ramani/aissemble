89bc010f6103e09d0c206e03d95dfb0d193f3599
Emily Wilkins <80470879+ewilkins-csi@users.noreply.github.com>
2025-05-21 10:43:52 -0500

    [#720] use python action instead of pyenv
    Mostly this just involves updating the `install_dependencies` action and
    updating Habushu's `usePyenv` to `false` if the `ci` profile is active.
    However, because the archetype test _also_ builds Habushu modules, it
    was trying to use `pyenv`.  So the foundation-archetype pom/test was
    updated to allow passing build profiles to the test script, and if the
    `ci` profile is active the module will pass `-Pci` to the script.
    
    Also fixes the `RuffTomlFileGenerationMigration` test. Apparently MacOs
    treats file paths case-insensitive (at least when accessed via Java)
    whereas the Ubuntu image we're using in CI does not.

--- foundation/foundation-archetype/test-project-archetype.sh
+++ foundation/foundation-archetype/test-project-archetype.sh
@@ -13,10 +13,12 @@
 set -o pipefail
 echo -e "\n\n **** TESTING ARCHETYPE GENERATION **** \n\n"
 
-if [ $# -ne 1 ]; then
+if [ $# -eq 0 ] || [ $# -gt 2 ]; then
     echo "Incorrect number of arguments. Usage:"
     echo "   test-project-archetype.sh <archetype_version>"
     exit 1
+elif [ -n "$2" ]; then
+  PROFILES="-P$2"
 fi
 
 mkdir -p target/temp
@@ -102,7 +104,7 @@ function runBuildAndApplyManualActions {
 
   # $deployOutputStart match at end ensures the match line isn't captured. NF ensures blank lines aren't captured.
   #TODO: remove docker.skip when Habushu containerization works
-  ./mvnw -Ddocker.skip clean install | \
+  ./mvnw -Ddocker.skip clean install $PROFILES | \
       tee >(awk "BEGIN {output=0} /$outputEnd/ {output=0} NF && output {print} /$deployOutputStart/ {output=1}">deploy.out ) | \
       tee >(awk "BEGIN {output=0} /$outputEnd/ {output=0} NF && output {print} /$helmfileOutputStart/ {output=1}">helmfile.out ) | \
       tee >(awk "BEGIN {output=0} /$outputEnd/ {output=0} NF && output {print} /$helmfileAppsOutputStart/ {output=1}">helmfile-apps.out ) \
@@ -141,6 +143,7 @@ function runBuildAndApplyManualActions {
 
 package='com.bah.aiops'
 echo "Using Project Version: $1"
+echo "Using build profiles: [$PROFILES]"
 echo -e "\nINFO: Generating a new project from the archetype\n"
 rm -rf test-generator
 mvn archetype:generate -B \
@@ -335,12 +338,12 @@ runBuildAndApplyManualActions
 
 #TODO: remove docker.skip when Habushu containerization works
 echo -e "\nINFO: Running final build to ensure success"
-./mvnw -Ddocker.skip clean install || { echo -e '\n\n\t**** MAVEN BUILD FAILED ****\n\n' ; exit 1; }
+./mvnw -Ddocker.skip clean install $PROFILES || { echo -e '\n\n\t**** MAVEN BUILD FAILED ****\n\n' ; exit 1; }
 
 echo -e "\nINFO: Running fermenter generation to check for left over manual actions\n"
 # NOTE: because fermenter results are cached, the build-cache will hide remaining manual actions that were missed in previous steps
 #TODO: remove docker.skip when Habushu containerization works
-./mvnw -Ddocker.skip clean generate-sources -Dmaven.build.cache.skipCache -Dfermenter.display.message.keys=true | tee >(awk '/WARNING/ {print}' > maven-build.log) \
+./mvnw -Ddocker.skip clean generate-sources $PROFILES -Dmaven.build.cache.skipCache -Dfermenter.display.message.keys=true | tee >(awk '/WARNING/ {print}' > maven-build.log) \
     || { echo -e '\n\n\t**** MAVEN BUILD FAILED ****\n\n' ; exit 1; }
 if grep -iq 'Manual action' maven-build.log; then
   echo -e "\n\n **** ERROR: Manual action still found in build **** \n    Look at **archetype/target/temp/test-generator/maven-build.log** to see what the problem was. \n\n"
